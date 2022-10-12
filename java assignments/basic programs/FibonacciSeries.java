3c0ac78086e083c7c11536ecd2904dab2cd8c497
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2022-10-12 20:41:35 +0530

    add entry statement

--- java assignments/basic programs/FibonacciSeries.java
+++ java assignments/basic programs/FibonacciSeries.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
+        System.out.print("Enter the number: ");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int one = 0;
         int two = 1;
         System.out.println(one);
         System.out.println(two);
-        for (int i = 1; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             int next = two + one;
             System.out.println(next);
             int temp = two;
