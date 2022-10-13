1a55544b213b3f2cb91faad58c7e23fa9e943f91
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2022-10-13 21:04:21 +0530

    palindrome check for string

---
+++ java assignments/basic programs/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Enter any string to check palindrome or not: ");
+        Scanner in = new Scanner(System.in);
+        String given = in.next();
+        System.out.println(ispalindrome(given));
+    }
+    static boolean ispalindrome(String given){
+        int start = 0;
+        int end = given.length() - 1;
+        while(start < end){
+            if (given.charAt(start) != given.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
